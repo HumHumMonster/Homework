f77da51365045bae9b7da1bb0939ed7f987b872d
ZhangHuanxin <40661210+HumHumMonster@users.noreply.github.com>
2019-05-05 01:07:39 +0800

    Create 二叉搜索树的单分支节点个数.cpp

---
+++ 二叉搜索树的单分支节点个数.cpp
@@ -0,0 +1,70 @@
+//二叉搜索树的单分支节点个数.cpp
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std ;
+
+typedef int ElemType ;
+
+struct BTreeNode
+{
+    ElemType data ;
+    BTreeNode* left ;
+    BTreeNode* right ;
+};
+
+
+void Insert(BTreeNode* &BST , const ElemType &item)
+{
+    if (BST == NULL)
+    {
+        BTreeNode*p = new BTreeNode ;
+        p -> data = item ;
+        p -> left = p -> right = NULL ;
+        BST = p ;
+    }
+    else if (item < BST -> data)
+        Insert (BST -> left , item) ;
+    else
+        Insert (BST -> right , item) ;
+}
+
+void Init(BTreeNode* &BST)
+{
+    BST = NULL ;
+}
+
+int Num_OneChildNode(BTreeNode* BST)
+{
+    if (BST == NULL)
+        return 0 ;
+    if (BST -> left == NULL && BST -> right == NULL)
+        return 0 ;
+    if (BST -> left == NULL)
+    {
+        return Num_OneChildNode(BST -> right) + 1 ;
+    }
+    else if (BST -> right == NULL)
+    {
+        return Num_OneChildNode(BST -> left) + 1 ;
+    }
+    else
+        return Num_OneChildNode(BST -> left) + Num_OneChildNode(BST -> right) ;
+}
+
+BTreeNode* root ;
+
+int main ()
+{
+
+    Init (root) ;
+    Insert(root , 5) ;
+    Insert(root , 4) ;
+    Insert(root , 6) ;
+    Insert(root , 3) ;
+    Insert(root , 2) ;
+    Insert(root , 8) ;
+    Insert(root , 7) ;
+    printf ("%d" , Num_OneChildNode(root)) ;
+}
