c774502dd6533a7bc99da5f3f65917c325af0f28
ZhangHuanxin <40661210+HumHumMonster@users.noreply.github.com>
2019-09-18 17:09:53 +0800

    Update zhongqiuyuebin.cpp

--- zhongqiuyuebin.cpp
+++ zhongqiuyuebin.cpp
@@ -1,4 +1,45 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <set>
+using namespace std ;
+#define LL long long
+const int MAXN = 1e5 + 10 ;
+
+set <LL> S ;
+LL n , k ;
+LL num[MAXN] ;
+
+
+int main ()
+{
+    FILE *fw ;
+    FILE *fr ;
+    fw = fopen("C:\\Users\\14714\\Desktop\\省赛题目\\数据\\中秋月饼\\10.in" , "w") ;
+    srand(time(NULL)) ;
+    n = 100000 ;
+    k = rand() * rand() ;
+    k %= (100000 + 1) ;
+    printf ("%lld %lld" , n , k) ;
+    fprintf (fw , "%lld %lld\n" , n , k) ;
+    for (int i = 1 ; i <= n ; ++i)
+    {
+        num[i] = rand() * rand() ;
+        num[i] %= (1000000 + 1) ;
+        if (S.count(num[i]))
+        {
+            --i ;
+            continue ;
+        }
+        fprintf (fw , "%lld\n" , num[i]) ;
+    }
+}
+
+
+
+
+
+#include <stdio.h>
 #include <algorithm>
 using namespace std ;
 
